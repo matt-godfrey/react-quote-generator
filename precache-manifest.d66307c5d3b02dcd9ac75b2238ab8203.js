23614681cf6752d0c9f6fe31721118964745fafc
matt-godfrey <godfrmat.mg@gmail.com>
2020-06-30 22:53:38 -0400

    Updates

---
+++ precache-manifest.d66307c5d3b02dcd9ac75b2238ab8203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a7bf4205a75dcf82cb614f64646d3f",
+    "url": "/react-quote-generator/index.html"
+  },
+  {
+    "revision": "927bfa8b9e94a7a3e566",
+    "url": "/react-quote-generator/static/css/main.cc48c090.chunk.css"
+  },
+  {
+    "revision": "0d84cbe920c58309a202",
+    "url": "/react-quote-generator/static/js/2.13a69ad1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quote-generator/static/js/2.13a69ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927bfa8b9e94a7a3e566",
+    "url": "/react-quote-generator/static/js/main.493fd2d1.chunk.js"
+  },
+  {
+    "revision": "4d3a52a683068f8e3475",
+    "url": "/react-quote-generator/static/js/runtime-main.3503b2e6.js"
+  }
+]);
